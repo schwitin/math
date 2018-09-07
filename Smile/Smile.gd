8bdc640a86b6c4d221390e7e35405c29fc0a6ca5
stalker <stalker@E460>
2018-09-07 14:59:30 +0200

    better smile

--- Smile/Smile.gd
+++ Smile/Smile.gd
@@ -1,38 +1,19 @@
-extends Node2D
+extends Sprite
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-var score=0
+var smile_sad
+var smile_jolly
 
-func set_score(_score):
-	if(_score > 10):
-		_score = 10
 	
-	if(_score < -10):
-		_score=-10
-	
-	score = _score * -1
-	update()
+
+func set_sad():
+	texture = smile_sad
+
+func set_jolly():
+	texture = smile_jolly
 
 
 
 func _ready():
-	set_score(10)
-
-func _draw():
-	var pos = Vector2(60,60)
-	draw_circle(pos, 60.0, Color('#ffff00') )
-	draw_circle(Vector2(30,30), 10.0, Color('#000000') )	
-	draw_circle(Vector2(90,30), 10.0, Color('#000000') )
-	draw_line( Vector2 (60,50), Vector2(60,70), Color('#000000'), 10.0)
-
-	draw_line( Vector2 (40,90), Vector2(80,90), Color('#000000'), 10.0)
-	
-	draw_line( Vector2 (30,90+score), Vector2(40,90), Color('#000000'), 10.0)
-	draw_line( Vector2 (90,90+score), Vector2(80,90), Color('#000000'), 10.0)
-	
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+	smile_sad = load("res://Smile/smilie-sad.png")
+	smile_jolly = load("res://Smile/smilie-jolly.png")
+	texture = smile_jolly
